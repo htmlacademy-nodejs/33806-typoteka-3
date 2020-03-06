a3e2c88adb13d54895233c6ad235cac240106e20
redVi <liberty195@gmail.com>
2020-03-06 22:19:05 +0700

    use express instead of http module

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -1,52 +1,26 @@
 'use strict';
 
-const http = require(`http`);
 const fs = require(`fs`).promises;
-const chalk = require(`chalk`);
+const express = require(`express`);
+const app = express();
 const {DEFAULT_PORT, MOCK_FILENAME, HttpCode, ResponseMessage, NodeExceptions} = require(`../constants`);
 
-const sendResponse = (res, statusCode, content) => {
-  const template = `
-    <!doctype html>
-      <html lang="ru">
-      <head>
-        <title>Posts</title>
-      </head>
-      <body>${content}</body>
-    </html>`.trim();
+app.use(express.urlencoded({extended: false}));
 
-  res.statusCode = statusCode;
-  res.writeHead(statusCode, {
-    'Content-Type': `text/html; charset=UTF-8`,
-  });
+app.get(`/posts`, async (req, res) => {
+  try {
+    const fileContent = await fs.readFile(MOCK_FILENAME, `utf-8`);
+    const mocks = JSON.parse(fileContent);
+    res.json(mocks);
+  } catch (err) {
+    if (err.code === NodeExceptions.ENOENT) {
+      res.status(HttpCode.NOT_FOUND).json({message: ResponseMessage.NOT_FOUND});
+      return;
+    }
 
-  res.end(template);
-};
-
-const onClientConnect = async (req, res) => {
-  switch (req.url) {
-    case `/`:
-      try {
-        const fileContent = await fs.readFile(MOCK_FILENAME, `utf-8`);
-        const mocks = JSON.parse(fileContent);
-        const content = mocks.map((post) => `<li>${post.title}</li>`).join(``);
-        sendResponse(res, HttpCode.OK, `<ul>${content}</ul>`);
-      } catch (err) {
-        if (err.code === NodeExceptions.ENOENT) {
-          sendResponse(res, HttpCode.NOT_FOUND, ResponseMessage.NOT_FOUND);
-          return;
-        }
-
-        sendResponse(res, HttpCode.INTERNAL_SERVER_ERROR, ResponseMessage.SERVER_ERROR);
-        console.log(chalk.red(`Server Error: `, err));
-      }
-
-      break;
-    default:
-      sendResponse(res, HttpCode.NOT_FOUND, ResponseMessage.NOT_FOUND);
-      break;
+    res.status(HttpCode.INTERNAL_SERVER_ERROR).json({message: ResponseMessage.SERVER_ERROR});
   }
-};
+});
 
 module.exports = {
   name: `--server`,
@@ -54,14 +28,7 @@ module.exports = {
     const [customPort] = args;
     const port = Number.parseInt(customPort, 10) || DEFAULT_PORT;
 
-    http.createServer(onClientConnect)
-      .listen(port)
-      .on(`listening`, (err) => {
-        if (err) {
-          return console.error(chalk.red(`Ошибка при создании сервера`, err));
-        }
-
-        return console.info(chalk.green(`Ожидаю соединений на http://localhost:${port}`));
-      });
+    app.listen(port,
+        () => console.log(`Server is running at: ${port}`));
   }
 };
